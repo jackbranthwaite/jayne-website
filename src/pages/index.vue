ef34baba9d0afe6e5b8411cc84ce371a2e4eaf0d
Jack <jack.branthwaite@gmail.com>
2021-09-07 21:21:43 +1200

    SaSS clean up

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,34 +1,13 @@
 <template>
-  <Intro>
-    <Heading content="jayne-website" />
-    <p>
-      Welcome to the
-      <a
-        href="https://formfunfunction.com/"
-        target="_blank"
-        rel="noreferrer"
-      >formfunfunction</a>
-      Nuxt starter project. Head to<Code>/src/pages/index.vue</Code> to get
-      stuck in.
-    </p>
-  </Intro>
+  <div class="container">
+    <h1 class="title">Hello World</h1>
+  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-import Intro from '@/components/intro/Intro.vue'
-import Code from '@/components/intro/Code.vue'
-import Heading from '@/components/intro/Heading.vue'
 
 export default Vue.extend({
   name: 'Home',
-  components: {
-    Intro,
-    Code,
-    Heading
-  },
-
-  head: {
-    title: 'Hello world!'
-  }
+  components: {}
 })
 </script>
