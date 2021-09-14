c351134b1e293f0ae8f1cb5d89bd615e878ec4f9
Jack <jack.branthwaite@gmail.com>
2021-09-14 20:30:59 +1200

    Progress on homepage

---
+++ src/pages/photography.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h2 class="title">photography</h2>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Photography',
+  components: {}
+})
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/styles/config';
+.title {
+  font-family: $cormorant;
+}
+</style>
